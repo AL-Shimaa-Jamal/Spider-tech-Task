6b44032a48e2bbf3f62ef57daf7c6881166dc3d8
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-08-19 00:12:11 +0300

    add all links in navbar

--- spidertech_task/src/components/NavBar.vue
+++ spidertech_task/src/components/NavBar.vue
@@ -1,26 +1,48 @@
 <template>
-  <div class="main shadow fixedNav">
+  <div class="main  fixedNav">
+    <!-- logo and language  -->
     <div
-      class="logo-lang d-flex justify-content-between align-items-center p-3"
-    >
+      class="logo-lang d-flex justify-content-between align-items-center p-3" >
       <div class="logo">
         <img src="/Logo.svg" alt="logo" />
       </div>
       <div class="lang">
         <select name="Language" id="">
-          <option value="English">English</option>
-          <option value="Arabic">Arabic</option>
+          <option class="p-2 mb-3 " value="English">English</option>
+          <option class="p-2 mb-3 " value="Arabic">Arabic</option>
         </select>
       </div>
     </div>
-    <div class="nav">
-      <ul class="list-unstyled d-flex" v-for="rout in Routers" :key="rout.name">
-        <li>
-          <router-link to="./" class="m-2 text-decoration-none text-dark">
-            {{ rout.meta.title }}</router-link
-          >
-        </li>
-      </ul>
+    <!-- links and media links -->
+    <div class="nav d-flex justify-content-between ms-5 me-5 ">
+      <div class="d-flex ">
+        <ul class="list-unstyled  " v-for="rout in Routers" :key="rout.name">
+          <li>
+            <router-link to="./" class="m-2 text-decoration-none text-dark">
+              {{ rout.meta.title }}</router-link
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="d-flex justify-content-between  ">
+        <ul class=" list-unstyled d-flex  social">
+          <li>
+            <a href="http://www.twitter.com" target="_blank" >
+              <img src="/twitter.svg" alt="">
+            </a>
+          </li>
+          <li>
+            <a href="http://www.facebook.com" target="_blank" >
+            <img src="/facebook.svg" alt="">
+          </a>
+          </li>
+          <li>
+            <a href="http://www.instgram.com" target="_blank" >
+              <img src="/instagram.svg" alt="">
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -44,4 +66,29 @@ export default {
   z-index: 1;
   width: 100vw;
 }
+select{
+  border: none;
+  outline: none;
+  font-size: 18px;
+  cursor: pointer;
+}
+select:hover{
+border-bottom:1px solid black ;
+font-weight: 500;
+letter-spacing: 1.2px;
+}
+.social li{
+cursor: pointer;
+  margin: 4%;
+
+}
+a img{
+  width: 20px;
+  transition: all .6s ease-in-out;
+}
+a img:hover{
+transform: scale(1.2,1.2);
+box-shadow: 5px 5px 10px gainsboro;
+}
+
 </style>
