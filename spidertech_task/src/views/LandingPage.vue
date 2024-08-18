c2e9d66d416a2f3a8d23b35b98fbf8076b7eb625
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-08-19 00:12:34 +0300

    start to build landing page

--- spidertech_task/src/views/LandingPage.vue
+++ spidertech_task/src/views/LandingPage.vue
@@ -1,8 +1,24 @@
 <template>
-    <h2>
-        hello in task 1
-    </h2>
+  <div class="main">
+    <div>
+        <img src="/image 26.svg" alt="">
+    </div>
+    <div>
+        <p>
+            Enhancing everything related to women's charm and allure is our mission.
+        </p>
+    </div>
+    <div>
+        <img src="/image 25.svg" alt="">
+    </div>
+  </div>
 </template>
  <style scoped>
+.main{
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+    align-items: center;
+}
 
 </style>
