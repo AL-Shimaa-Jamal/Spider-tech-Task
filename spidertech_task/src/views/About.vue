0908b001fd55bb07f2990546c0758c3d1746ca82
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-08-19 14:26:35 +0300

    nearly finish about component
    here build about component and try to make it responsive with all screens

--- spidertech_task/src/views/About.vue
+++ spidertech_task/src/views/About.vue
@@ -4,7 +4,7 @@
       <img src="/image 44.svg" alt="" />
     </div>
     <div class="aboutContent ms-5 ps-4">
-      <h3 class="text-capitalize d-block  mb-5">about us</h3>
+      <h3 class="text-capitalize d-block mb-5">about us</h3>
       <p class="d-block pt-5 w-75">
         From ancient times to the present day, our journey continues to bring
         the best to the modern woman. We draw inspiration from our rich heritage
@@ -26,35 +26,54 @@
 </template>
 <style scoped>
 .About {
-  margin: 7% 0;
+  margin: 17% 0;
 }
 h3 {
   font-size: 140px;
   font-weight: 100;
   line-height: 128.8px;
-  text-align: left;
 }
 p {
   font-size: 18px;
   font-weight: 400;
   line-height: 28px;
-  text-align: left;
 }
 h4 {
   font-size: 48px;
   font-weight: 700;
   line-height: 44.16px;
-  text-align: left;
   margin: 5% 0;
 }
 ul li {
-  width: 640px;
-  height:48px;
-  padding: 10px 16px 10px 16px;
+  width: 80%;
+  padding: 10px 16px;
   margin: 2% 0;
-  border-radius: 12px 0px 0px 0px;
-  border: 0px 0px 1px 1px;
-  opacity: 0px;
+  border-top-left-radius: 12px;
   background: rgba(244, 244, 244, 1);
+  transition: all .5s ease-in-out;
+}
+ul li:hover {
+transform: scale(1.1,1.1);
+}
+/*  responce */
+@media screen and (max-width: 1100px) {
+  .About {
+    margin: 30% 0;
+  }
+  h3 {
+    font-size: 70px;
+    font-weight: 100;
+    line-height: 100.8px;
+  }
+
+  h4 {
+    font-size: 29px;
+  }
+}
+@media screen and (max-width: 815px) {
+  h3 {
+    font-size: 70px;
+    line-height: 11.8px;
+  }
 }
 </style>
