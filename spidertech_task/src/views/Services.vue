1aefa4cc0b2e9fbb450100480133c8c0f72fe76e
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-08-19 15:19:22 +0300

    add service component and byil some basic in it
    build title ,description and add some style to make design as a proto type

--- spidertech_task/src/views/Services.vue
+++ spidertech_task/src/views/Services.vue
@@ -1,5 +1,80 @@
 <template>
-    <h2 class=" text-center fw-bold m-5 ">
-        services
-    </h2>
+<section class="main ">
+<div class="d-flex justify-content-center  p-2 m-4">
+    <img src="/Frame 120.svg" alt="">
+</div>
+<div class="container p-3 m-3 d-flex justify-content-center">
+    <p class="w-75 text-center">
+ GOOCHIE is a leading Chinese brand in permanent makeup, founded in 1996. It offers a variety of devices and pigments to meet global market needs, with a wide network of agents in over 64 countries. The brand focuses on providing high-quality products and professional, safe services to users worldwide​.
+    </p>
+</div>
+<div class=" container bg-light slid ">
+<h3>
+    Duogel Product Showcase
+</h3>
+<ul class="list-unstyled d-flex justify-content-between   p-2 ">
+    <li> 
+        <a href="#">Devices</a>
+    </li>
+    <li> 
+        <a href="#">UV gel nail polishes</a>
+    </li>
+    <li> 
+        <a href="#">Nail art</a>
+    </li>
+    <li> 
+        <a href="#">Accessories</a>
+    </li>
+    <li> 
+        <a href="#">UV Gels</a>
+    </li>
+    <li> 
+        <a href="#">Liquids / Preparations</a>
+    </li>
+    <li> 
+        <a href="#">Care</a>
+    </li>
+    <li> 
+        <a href="#">Disinfection / Hygiene</a>
+    </li>
+    <li> 
+        <a href="#">Acrylics</a>
+    </li>
+    <li> 
+        <a href="#">Outlet</a>
+    </li>
+    <li> 
+        <a href="#">Collections</a>
+    </li>
+
+</ul>
+<div>
+    <img src="/new-duo-gabki-do-ombre.webp.svg" alt="new-duo-gabki-do-ombre.webp">
+</div>
+</div>
+</section>
 </template>
+<style scoped>
+.main{
+background-color:rgba(222, 235, 246, 1) ;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 20px;
+}
+.slid{
+    border-radius: 20px;
+    padding: 2%;
+    overflow-x:scroll ;
+}
+li{
+    margin:0 10px;
+
+}
+a{
+    text-decoration: none;
+    color: rgba(0, 0, 0, 0.3);
+    margin: 0 10px;
+}
+</style>
