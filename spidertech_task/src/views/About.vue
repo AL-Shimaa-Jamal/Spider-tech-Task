deacaeae88b88bebc6d96ff001e7a35656dd5e4e
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-08-19 12:45:10 +0300

    start build about page

--- spidertech_task/src/views/About.vue
+++ spidertech_task/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-    <h2 class=" text-center fw-bold m-5 ">
-        about
-    </h2>
+   <div class="d-flex justify-content-between align-items-center About">
+    <div>
+        <img src="/image 44.svg" alt="">
+    </div>
+    <div class="text-start">
+        <h3 class=" text-capitalize">about us</h3>
+    </div>
+   </div>
 </template>
+<style scoped>
+.About{
+    margin: 7% 0;
+}
+</style>
