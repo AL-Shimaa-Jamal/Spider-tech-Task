da4b5eb4e8b8b3673856c456ecb0ba618502c88b
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-08-19 13:10:46 +0300

    nearly finish about page

--- spidertech_task/src/views/About.vue
+++ spidertech_task/src/views/About.vue
@@ -1,15 +1,60 @@
 <template>
-   <div class="d-flex justify-content-between align-items-center About">
-    <div>
-        <img src="/image 44.svg" alt="">
+  <div class="d-flex justify-content-between About">
+    <div class="d-none d-lg-block">
+      <img src="/image 44.svg" alt="" />
     </div>
-    <div class="text-start">
-        <h3 class=" text-capitalize">about us</h3>
+    <div class="aboutContent ms-5 ps-4">
+      <h3 class="text-capitalize d-block  mb-5">about us</h3>
+      <p class="d-block pt-5 w-75">
+        From ancient times to the present day, our journey continues to bring
+        the best to the modern woman. We draw inspiration from our rich heritage
+        and blend it with contemporary touches to offer you a diverse range of
+        products that reflect the elegance and sophistication of the past with a
+        modern twist.
+      </p>
+      <div>
+        <h4>Values</h4>
+        <ul class="list-unstyled">
+          <li>Sustainability</li>
+          <li>Women's Empowerment</li>
+          <li>Innovation and Creativity</li>
+          <li>Excellent Customer Service</li>
+        </ul>
+      </div>
     </div>
-   </div>
+  </div>
 </template>
 <style scoped>
-.About{
-    margin: 7% 0;
+.About {
+  margin: 7% 0;
+}
+h3 {
+  font-size: 140px;
+  font-weight: 100;
+  line-height: 128.8px;
+  text-align: left;
+}
+p {
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 28px;
+  text-align: left;
+}
+h4 {
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 44.16px;
+  text-align: left;
+  margin: 5% 0;
+}
+ul li {
+  width: 640px;
+  height:48px;
+  padding: 10px 16px 10px 16px;
+  margin: 2% 0;
+  border-radius: 12px 0px 0px 0px;
+  border: 0px 0px 1px 1px;
+  opacity: 0px;
+  background: rgba(244, 244, 244, 1);
 }
 </style>
