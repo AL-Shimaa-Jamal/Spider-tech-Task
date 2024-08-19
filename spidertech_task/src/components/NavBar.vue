0aa18117d27f5a6c2060130144b466cf35b5c39d
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-08-19 12:44:11 +0300

    make nav response

--- spidertech_task/src/components/NavBar.vue
+++ spidertech_task/src/components/NavBar.vue
@@ -1,51 +1,73 @@
 <template>
-  <div class="main  fixedNav">
+  <div class="main fixedNav">
     <!-- logo and language  -->
     <div
-      class="logo-lang d-flex justify-content-between align-items-center p-2" >
+      class="logo-lang d-flex justify-content-between align-items-center p-2"
+    >
       <div class="logo">
         <img src="/Logo.svg" alt="logo" width="90%" />
       </div>
       <div class="lang">
         <select name="Language" id="">
-          <option class="p-2 mb-3 " value="English">English</option>
-          <option class="p-2 mb-3 " value="Arabic">Arabic</option>
+          <option class="p-2 mb-3" value="English">English</option>
+          <option class="p-2 mb-3" value="Arabic">Arabic</option>
         </select>
       </div>
     </div>
     <!-- links and media links -->
-    <div class="nav d-flex justify-content-between ms-3 me-3 ">
-      <div class="d-flex ">
-        <ul class="list-unstyled  " v-for="rout in Routers" :key="rout.name">
-          <li>
-            <router-link to="./" class="m-2 text-decoration-none text-dark">
-              {{ rout.meta.title }}</router-link
-            >
-          </li>
-        </ul>
-      </div>
-      <div class="d-flex justify-content-between  ">
-        <ul class=" list-unstyled d-flex  social">
-          <li>
-            <a href="http://www.twitter.com" target="_blank" >
-              <img src="/twitter.svg" alt="">
-            </a>
-          </li>
-          <li>
-            <a href="http://www.facebook.com" target="_blank" >
-            <img src="/facebook.svg" alt="">
-          </a>
-          </li>
-          <li>
-            <a href="http://www.instgram.com" target="_blank" >
-              <img src="/instagram.svg" alt="">
-            </a>
-          </li>
-        </ul>
+    <div class="navbar navbar-expand-lg ms-3 me-3">
+      <button
+        class="navbar-toggler ms-auto"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#Content"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="Content">
+        <div class="d-lg-flex justify-content-between  w-100">
+          <!-- Router Links -->
+          <div class="d-lg-flex">
+            <ul class="list-unstyled d-lg-flex">
+              <li v-for="rout in Routers" :key="rout.name">
+                <router-link
+                  :to="rout.path"
+                  class="m-2 text-decoration-none text-dark nav-link"
+                >
+                  {{ rout.meta.title }}
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <!-- Social Media Links -->
+          <div class="d-flex ">
+            <ul class="list-unstyled d-flex social">
+              <li>
+                <a href="http://www.twitter.com" target="_blank">
+                  <img src="/twitter.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="http://www.facebook.com" target="_blank">
+                  <img src="/facebook.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="http://www.instgram.com" target="_blank">
+                  <img src="/instagram.svg" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -67,31 +89,29 @@ export default {
   z-index: 100;
   width: 100%;
   background-color: white;
-  box-shadow: 0px 4px 2px -2px gray; 
+  box-shadow: 0px 4px 2px -2px gray;
 }
-select{
+select {
   border: none;
   outline: none;
   font-size: 18px;
   cursor: pointer;
 }
-select:hover{
-border-bottom:1px solid black ;
-font-weight: 500;
-letter-spacing: 1.2px;
+select:hover {
+  border-bottom: 1px solid black;
+  font-weight: 500;
+  letter-spacing: 1.2px;
 }
-.social li{
-cursor: pointer;
+.social li {
+  cursor: pointer;
   margin: 4%;
-
 }
-a img{
+a img {
   width: 20px;
-  transition: all .6s ease-in-out;
+  transition: all 0.6s ease-in-out;
 }
-a img:hover{
-transform: scale(1.2,1.2);
-box-shadow: 5px 5px 10px gainsboro;
+a img:hover {
+  transform: scale(1.2, 1.2);
+  box-shadow: 5px 5px 10px gainsboro;
 }
-
 </style>
